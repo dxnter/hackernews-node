4069cf170b8d210f26bf994ebbbd22f8486e6a4e
Danny <contact@danfoster.io>
2018-08-01 20:04:48 -0500

    Moved typeDefs to schema.graphql

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  },
+];
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
